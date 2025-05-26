4605bb5cd8a2aad28324f2b56c8834fea099e725
aaadi02 <adityadakhane011@gmail.com>
2025-05-26 19:52:19 +0530

    oisidshncvkhjdbf

--- models/Subject.js
+++ models/Subject.js
@@ -7,7 +7,6 @@
 
 // // module.exports = mongoose.model('Subject', subjectSchema);
 
-
 // const mongoose = require('mongoose');
 
 // const subjectSchema = new mongoose.Schema({
@@ -17,19 +16,19 @@
 
 // module.exports = mongoose.model('Subject', subjectSchema);
 
-
-
-
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const subjectSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  department: { type: mongoose.Schema.Types.ObjectId, ref: 'Department', required: true },
+  department: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Department",
+    required: true,
+  },
 });
 
-// Use `mongoose.models` to check if the model already exists, 
+// Use `mongoose.models` to check if the model already exists,
 // otherwise create a new model
-module.exports = mongoose.models.Subject || mongoose.model('Subject', subjectSchema);
-
-
-
+module.exports =
+  mongoose.models.Subject || mongoose.model("Subject", subjectSchema);
+// module.exports = mongoose.model("Subject", subjectSchema);
